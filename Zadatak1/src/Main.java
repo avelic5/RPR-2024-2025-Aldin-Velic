a5579ede2244db509af608ab252af839ae506e86
avelic5 <avelic5@etf.unsa.ba>
2024-10-23 15:50:14 +0100

    Uradjen prvi zadatak prve LV

--- Zadatak1/src/Main.java
+++ Zadatak1/src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
+
+            System.out.println("Unesite n: ");
+            int n=0;
+            boolean x=true;
+            Scanner ulaz=new Scanner(System.in);
+            do{
+                n=ulaz.nextInt();
+                if(n<2){System.out.println("Nije moguce izvrsiti izracunavanje prostih brojeva");
+                    return;}
+                if(n>500)System.out.println("Ponovite unos");
+           } while(n>500);
+            for(int i=2;i<2*n;i=i+1){
+                for(int j=2;j<2*n/j;j=j+1){
+                    if(i%j==0){x=false;break;}}
+                        if(x||i==2||i==3)System.out.print(i+" ");
+
+                        x=true;
+
+
+            }
+
+
     }
 }
